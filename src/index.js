162ed0ea737452c24555a95f5d2b7f4ef95b588c
Waqar <ahmedwaqar.maliks@gmail.com>
2020-08-07 15:39:48 -0400

    React + Redux Login App

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {createStore} from 'redux'
 import { Provider } from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import allReducers from './reducers';
+import { store } from './store';
 
-const store = createStore(
-  allReducers,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-
-// store.subscribe(() => console.log(store.getState()));
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
